df340aa4a76a242c5a9010e8e19596acb19a2ad9
Ana Mendez <ana.mendezr@uniagustiniana.edu.co>
2023-03-06 10:18:46 -0500

    Correcciones

--- app/src/main/java/com/example/taller3_mendez_useche/Activity2.java
+++ app/src/main/java/com/example/taller3_mendez_useche/Activity2.java
@@ -1,14 +1,26 @@
 package com.example.taller3_mendez_useche;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class Activity2 extends AppCompatActivity {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Activity2 extends AppCompatActivity{
     EditText txtCodigo, txtCorreo;
     Button btnVerificar;
 
@@ -17,22 +29,46 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-
-
         txtCodigo = findViewById(R.id.txtCodigo);
         txtCorreo = findViewById(R.id.txtCorreo);
         btnVerificar = findViewById(R.id.btnVerificar);
+
+        btnVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Estudiante estudiante = new Estudiante();
+                estudiante.setCodigo(txtCodigo.getText().toString());
+                estudiante.setCorreo(txtCorreo.getText().toString());
+
+                try {
+                    if(txtCorreo.getText().toString().contains("@gmail.com") || txtCorreo.getText().toString().contains("@hotmail.com") || txtCorreo.getText().toString().contains("@uniagustiniana.edu.co")) {
+                        if (txtCodigo.getText().toString().isEmpty() || txtCorreo.getText().toString().isEmpty()) {
+                            txtCodigo.setText("");
+                            txtCorreo.setText("");
+                            Toast.makeText(Activity2.this, "Todos los campos son requeridos", Toast.LENGTH_SHORT).show();
+                        } else {
+                            FileOutputStream file = openFileOutput("estudiante.txt", Context.MODE_PRIVATE);
+                            ObjectOutputStream serializacion = new ObjectOutputStream(file);
+                            serializacion.writeObject(estudiante);
+                            serializacion.close();
+
+                            Intent intent = new Intent(Activity2.this, Activity3.class);
+                            startActivity(intent);
+                        }
+                    }
+                    else
+                    {
+                        txtCodigo.setText("");
+                        txtCorreo.setText("");
+                        Toast.makeText(Activity2.this, "El correo no es válido", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
     }
-
-    public void Verificar(View view){
-        Estudiante estudiante = new Estudiante();
-        estudiante.setCodigo(txtCodigo.getText().toString());
-        estudiante.setCorreo(txtCorreo.getText().toString());
-
-        Intent intent= new Intent(this, Activity3.class);
-        intent.putExtra("Estudiante", estudiante);
-
-        startActivity(intent);
-    }
-
 }
--- app/src/main/java/com/example/taller3_mendez_useche/Activity3.java
+++ app/src/main/java/com/example/taller3_mendez_useche/Activity3.java
@@ -2,26 +2,43 @@ package com.example.taller3_mendez_useche;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class Activity3 extends AppCompatActivity {
-    EditText txtCodigo,txtCorreo, edNota;
+    EditText edCodigo,edCorreo, edNota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
 
-        txtCodigo = findViewById(R.id.edCodigo);
-        txtCorreo = findViewById(R.id.edCorreo);
+        edCodigo = findViewById(R.id.edCodigo);
+        edCorreo = findViewById(R.id.edCorreo);
         edNota = findViewById(R.id.edNota);
 
-        Estudiante estudiante =(Estudiante) getIntent().getSerializableExtra("Estudiante");
-        txtCodigo.setText(estudiante.getCodigo());
-        txtCorreo.setText(estudiante.getCorreo());
-        edNota.setText(estudiante.getNota());
+        try {
+            FileInputStream file = openFileInput("estudiante.txt");
+            ObjectInputStream deserializacion = new ObjectInputStream(file);
+            Estudiante info_estudiante = (Estudiante) deserializacion.readObject();
+            deserializacion.close();
 
+            edCodigo.setText(info_estudiante.getCodigo());
+            edCorreo.setText(info_estudiante.getCorreo());
+            edNota.setText(info_estudiante.getNota());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        edCodigo.setEnabled(false);
+        edCorreo.setEnabled(false);
         edNota.setEnabled(false);
     }
 }
